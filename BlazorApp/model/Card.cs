0a9b2874245f3c036f3d7514d4cf8e778eb9a6a6
Y-Baker <yuossefahmedgaber@gmail.com>
2024-08-23 20:52:24 +0300

    Card Component

---
+++ BlazorApp/model/Card.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp.model
+{
+    public class CardData
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? LinkRout {  get; set; }
+        public string? LinkText { get; set; }
+        public string? ImgSrc { get; set; }
+
+        public CardData(string title, string? descr = null, string? linkRout = null, string? linkText = null, string? ImgSrc = null)
+        {
+            this.Id = $"card-{Guid.NewGuid().ToString()}";
+            this.Title = title;
+            this.Description = descr;
+            this.LinkRout = linkRout;
+            this.LinkText = linkText;
+            this.ImgSrc = ImgSrc;
+        }
+    }
+}
