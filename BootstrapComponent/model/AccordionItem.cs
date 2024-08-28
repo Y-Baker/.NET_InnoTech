dae9312de1dd1a6f8399ef08ebde3c81cf693691
Y-Baker <yuossefahmedgaber@gmail.com>
2024-08-28 21:58:50 +0300

    Last Task

---
+++ BootstrapComponent/model/AccordionItem.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp.Models;
+
+public class AccordionItem
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+
+    public AccordionItem(int id, string? title = null, string? description = null)
+    {
+        this.Id = id;
+        this.Title = title;
+        this.Description = description;
+    }
+}
---
+++ BootstrapComponent/wwwroot/js/app.js
@@ -0,0 +1,10 @@
+﻿function startAccordion(accordionId) {
+    const myaccordion = document.getElementById(accordionId);
+    const accordionItems = myaccordion.querySelectorAll('.accordion-collapse');
+
+    accordionItems.forEach(item => {
+        new bootstrap.Collapse(item, {
+            toggle: false
+        });
+    });
+}
